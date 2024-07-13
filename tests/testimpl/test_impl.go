4a8878efc1c585922b49f6145388795a9171216f
Bryce Lynn <bryce.lynn@nttdata.com>
2024-07-12 17:17:49 -0700

    with working tests

--- tests/testimpl/test_impl.go
+++ tests/testimpl/test_impl.go
@@ -7,27 +7,33 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/applicationautoscaling"
+	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/launchbynttdata/lcaf-component-terratest/types"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestAutoscalingTarget(t *testing.T, ctx types.TestContext) {
 	appAutoscalingClient := applicationautoscaling.NewFromConfig(GetAWSConfig(t))
-	//serviceId := terraform.Output(t, ctx.TerratestTerraformOptions(), "service_id")
+	targetId := terraform.Output(t, ctx.TerratestTerraformOptions(), "autoscaling_target_id")
 
 	output, err := appAutoscalingClient.DescribeScalableTargets(context.TODO(), &applicationautoscaling.DescribeScalableTargetsInput{
 		ServiceNamespace: "ecs",
-		//ResourceIds:      []string{serviceId},
+		ResourceIds:      []string{targetId},
 	})
 	if err != nil {
 		t.Errorf("Unable to describe scalable targets, %v", err)
 	}
-	//autoscalingTarget := output.ScalableTargets[0]
 
-	t.Run("TestTargetExists", func(t *testing.T) {
-		require.NotEmpty(t, output.ScalableTargets, "No scalable targets found")
+	t.Run("TestOnlyOneTargetExists", func(t *testing.T) {
+		assert.Len(t, output.ScalableTargets, 1, "Expected one scalable target, got %d", len(output.ScalableTargets))
 	})
 
+	autoscalingTarget := output.ScalableTargets[0]
+
+	t.Run("TestTargetHasCorrectResourceID", func(t *testing.T) {
+		require.Equal(t, targetId, *autoscalingTarget.ResourceId, "Expected resource ID to be %s, got %s", targetId, *autoscalingTarget.ResourceId)
+	})
 }
 
 func GetAWSConfig(t *testing.T) (cfg aws.Config) {
